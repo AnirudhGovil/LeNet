c6231bc5117f74380f237d8c27ec7b194ee5400e
ankithvarun <ankith.varun@research.iiit.ac.in>
2022-11-29 17:34:24 +0530

    Fixed LeNet1

--- LeNet1/model.py
+++ LeNet1/model.py
@@ -7,11 +7,13 @@ class LeNet1(torch.nn.Module):
         super(LeNet1, self).__init__()
         self.conv1 = torch.nn.Conv2d(1, 4, 5)
         self.conv2 = torch.nn.Conv2d(4, 12, 5)
+        self.fc = torch.nn.Linear(12 * 4 * 4, 10)
 
     def forward(self, x):
         x = torch.nn.functional.avg_pool2d(torch.nn.functional.relu(self.conv1(x)), 2)
         x = torch.nn.functional.avg_pool2d(torch.nn.functional.relu(self.conv2(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
+        x = self.fc(x)
         return x
 
     def num_flat_features(self, x):
