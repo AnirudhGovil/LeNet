a5d194a7022777247e68b5b7a814cc63cd6f9cd8
ankithvarun <ankith.varun@research.iiit.ac.in>
2022-12-02 14:27:41 +0530

    Integrated NN classifiers to dashboard for digit recognition

--- LeNet5/One_Hidden_Layer_FCMNN.py
+++ LeNet5/One_Hidden_Layer_FCMNN.py
@@ -1,4 +1,3 @@
-from model import LeNet5
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -11,11 +10,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+class OneFCMNN(torch.nn.Module):
+    def __init__(self):
+        super(OneFCMNN, self).__init__()
+        self.fc1 = torch.nn.Linear(400, 300)
+        self.fc2 = torch.nn.Linear(300, 10)
+
+    def forward(self, x):
+        x = x.view(-1, self.num_flat_features(x))
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
 # Predict the class of a single image using LeNet5
 
 def predict(image_path, model, device):
     # Define the transform to normalize the data
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    transform = transforms.Compose([transforms.Resize(20) ,transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     # Load the image
     image = Image.open(image_path)
     # Convert the image to grayscale
@@ -40,9 +58,9 @@ def predict(image_path, model, device):
 # Print the predicted class of the image
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Load the saved model with the best performance
-model = LeNet5()
+model = OneFCMNN()
 if(device.type == 'cpu'):
-    model.load_state_dict(torch.load('LeNet_5.pth', map_location='cpu'))
+    model.load_state_dict(torch.load('One_Hidden_Layer_FCMNN.pth', map_location='cpu'))
 else:
-    model.load_state_dict(torch.load('LeNet_5.pth'))
+    model.load_state_dict(torch.load('One_Hidden_Layer_FCMNN.pth'))
 print(predict("images/number.png", model, device))
