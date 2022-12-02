1980f4eee3c12ef8e70b528fbe6f06942c5abbc5
siddharth-mavani <siddharthmavani555@gmail.com>
2022-12-02 09:16:25 +0300

    Added dashboard menu for alphabets also

--- Alphabet_Recognition/LeNet5/interface.py
+++ Alphabet_Recognition/LeNet5/interface.py
@@ -14,6 +14,8 @@ def save_drawn_image(uploadedfile):
   im = Image.fromarray(uploaded_file)
   im.save(os.path.join("images","number.png"))
 
+alpa_dict = {0:"N/A", 1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I", 10:"J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T", 21:"U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
+
 
 st.title("LeNet-5")
 
@@ -55,13 +57,26 @@ canvas_result = st_canvas(
 
 # Do something interesting with the image data and paths
 if canvas_result.image_data is not None:
+  option = st.selectbox( 'Select Classifier', ('LeNet 5', 'LeNet 1', 'LeNet 4', 'Booseted LeNet 4', 'One-Hidden-Layer FCMNN', 'Two-Hidden-Layer FCMNN', 'KNN', 'Linear Classifier', 'Polynomial Classifier', 'RBF Network', 'SVM'))
   if st.button('Predict'):
+    st.write("option is: ",option)
     uploaded_file = canvas_result.image_data
     if uploaded_file is not None:
     # save the image
       save_drawn_image(uploaded_file)
       with st.spinner('Identifying...'):
           # print the output of predict.py
-          st.write("LeNet thinks this digit is: ",int(subprocess.check_output(["python", "predict.py"])))
+          if(option == 'LeNet 5'):
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "predict.py"]))])
+          elif(option == 'KNN'):
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "KNN.py"]))])
+          elif(option == 'Linear Classifier'):
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "Linear_Classifier.py"]))])
+          elif(option == 'Polynomial Classifier'):  
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "Polynomial_Classifier.py"]))])
+          elif(option == 'RBF Network'):
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "RBF_Network.py"]))])
+          elif(option == 'SVM'):
+            st.write("LeNet thinks this alphabet is: ",alpa_dict[int(subprocess.check_output(["python", "SVM.py"]))])
 
 
